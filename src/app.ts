d6623617944cdce570841b4ce79684162d9307ef
Luis V <luisvinicius0906@gmail.com>
2024-04-05 17:20:28 -0300

    add login with jwt route; add jsonwebtoken deps

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,9 @@ import { z } from 'zod';
 import { db } from './database';
 import { users } from './schema';
 import { eq } from 'drizzle-orm';
-import { hash } from 'bcrypt';
+import { compare, hash } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+import { env } from './env';
 
 export const app = fastify();
 
@@ -24,7 +26,7 @@ app.post('/register-user', async (req, reply) => {
 		console.error('Invalid register user body.', body.error.format());
 
 		return reply
-			.status(404)
+			.status(400)
 			.send({ message: 'Invalid register user body.', errors: body.error.format() });
 	}
 
@@ -50,4 +52,41 @@ app.post('/register-user', async (req, reply) => {
 		.where(eq(users.email, email));
 
 	return reply.send({ message: 'success', user });
+});
+
+app.post('/login-with-jwt', async (req, reply) => {
+	const registerUserSchema = z.object({
+		email: z.string().email(),
+		password: z.string(),
+	});
+
+	const body = registerUserSchema.safeParse(req.body);
+
+	if (!body.success) {
+		console.error('Invalid register user body.', body.error.format());
+
+		return reply
+			.status(400)
+			.send({ message: 'Invalid register user body.', errors: body.error.format() });
+	}
+
+	const { email, password } = body.data;
+
+	const [user] = await db.select().from(users).where(eq(users.email, email));
+
+	if (!user) {
+		return reply.status(404).send({ message: 'Email or password invalid!' });
+	}
+
+	const passwordMatches = await compare(password, user.password);
+
+	if (!passwordMatches) {
+		return reply.status(400).send({ message: 'Email or password invalid!' });
+	}
+
+	const token = sign({ name: user.name, email: user.email, issueAt: Date.now() }, env.JWT_SECRET, {
+		expiresIn: '60 * 60',
+	});
+
+	return reply.send({ message: 'Logged In!', token });
 });
