4e26f95d55f5e3c8e11738743e65ba22f45fdac3
Luis V <luisvinicius0906@gmail.com>
2024-04-05 17:04:45 -0300

    add register user route; add bcrypt for pw encryption

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,53 @@
 import fastify from 'fastify';
+import { z } from 'zod';
+import { db } from './database';
+import { users } from './schema';
+import { eq } from 'drizzle-orm';
+import { hash } from 'bcrypt';
 
 export const app = fastify();
+
+app.get('/', (req, reply) => {
+	return reply.send({ message: 'Hello to new world' });
+});
+
+app.post('/register-user', async (req, reply) => {
+	const registerUserSchema = z.object({
+		name: z.string(),
+		email: z.string().email(),
+		password: z.string(),
+	});
+
+	const body = registerUserSchema.safeParse(req.body);
+
+	if (!body.success) {
+		console.error('Invalid register user body.', body.error.format());
+
+		return reply
+			.status(404)
+			.send({ message: 'Invalid register user body.', errors: body.error.format() });
+	}
+
+	const { email, password, name } = body.data;
+
+	const userExists = await db.select().from(users).where(eq(users.email, email));
+
+	if (userExists) {
+		return reply.status(400).send({ message: 'Email already exists' });
+	}
+
+	await db.insert(users).values({ email: email, name: name, password: await hash(password, 10) });
+
+	const user = db
+		.select({
+			id: users.id,
+			email: users.email,
+			name: users.name,
+			createdAt: users.createdAt,
+			updatedAt: users.updatedAt,
+		})
+		.from(users)
+		.where(eq(users.email, email));
+
+	return reply.send({ message: 'success', user });
+});
