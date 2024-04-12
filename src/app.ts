b0eabbb8895de574bed6046a65ea74ce219d2a6f
Luis V <luisvinicius0906@gmail.com>
2024-04-12 10:30:06 -0300

    add try catch block for better error handling

--- src/app.ts
+++ src/app.ts
@@ -26,38 +26,44 @@ app.post('/register-user', async (req, reply) => {
 		password: z.string(),
 	});
 
-	const body = registerUserSchema.safeParse(req.body);
+	try {
+		const body = registerUserSchema.safeParse(req.body);
 
-	if (!body.success) {
-		console.error('Invalid register user body.', body.error.format());
+		if (!body.success) {
+			console.error('Invalid register user body.', body.error.format());
 
-		return reply
-			.status(400)
-			.send({ message: 'Invalid register user body.', errors: body.error.format() });
+			return reply
+				.status(400)
+				.send({ message: 'Invalid register user body.', errors: body.error.format() });
+		}
+
+		const { email, password, name } = body.data;
+
+		const [userExists] = await db.select().from(users).where(eq(users.email, email));
+
+		if (userExists) {
+			return reply.status(400).send({ message: 'Email already exists' });
+		}
+
+		await db.insert(users).values({ email: email, name: name, password: await hash(password, 10) });
+
+		const [user] = await db
+			.select({
+				id: users.id,
+				email: users.email,
+				name: users.name,
+				createdAt: users.createdAt,
+				updatedAt: users.updatedAt,
+			})
+			.from(users)
+			.where(eq(users.email, email));
+
+		return reply.send({ message: 'success', user });
+	} catch (err) {
+		console.error(err);
+
+		return reply.status(500).send({ message: 'Internal server error' });
 	}
-
-	const { email, password, name } = body.data;
-
-	const userExists = await db.select().from(users).where(eq(users.email, email));
-
-	if (userExists) {
-		return reply.status(400).send({ message: 'Email already exists' });
-	}
-
-	await db.insert(users).values({ email: email, name: name, password: await hash(password, 10) });
-
-	const user = db
-		.select({
-			id: users.id,
-			email: users.email,
-			name: users.name,
-			createdAt: users.createdAt,
-			updatedAt: users.updatedAt,
-		})
-		.from(users)
-		.where(eq(users.email, email));
-
-	return reply.send({ message: 'success', user });
 });
 
 app.post('/login-with-jwt', async (req, reply) => {
@@ -66,38 +72,48 @@ app.post('/login-with-jwt', async (req, reply) => {
 		password: z.string(),
 	});
 
-	const body = registerUserSchema.safeParse(req.body);
+	try {
+		const body = registerUserSchema.safeParse(req.body);
 
-	if (!body.success) {
-		console.error('Invalid register user body.', body.error.format());
+		if (!body.success) {
+			console.error('Invalid register user body.', body.error.format());
 
-		return reply
-			.status(400)
-			.send({ message: 'Invalid register user body.', errors: body.error.format() });
+			return reply
+				.status(400)
+				.send({ message: 'Invalid register user body.', errors: body.error.format() });
+		}
+
+		const { email, password } = body.data;
+
+		const [user] = await db.select().from(users).where(eq(users.email, email));
+
+		if (!user) {
+			return reply.status(404).send({ message: 'Email or password invalid!' });
+		}
+
+		const passwordMatches = await compare(password, user.password);
+
+		if (!passwordMatches) {
+			return reply.status(400).send({ message: 'Email or password invalid!' });
+		}
+
+		const token = sign(
+			{ name: user.name, email: user.email, issueAt: Date.now() },
+			env.JWT_SECRET,
+			{
+				expiresIn: '1h',
+			}
+		);
+
+		return reply.send({ message: 'Logged In!' }).setCookie('Authorization', `Bearer ${token}`, {
+			httpOnly: true,
+			secure: true,
+		});
+	} catch (err) {
+		console.error(err);
+
+		return reply.status(500).send({ message: 'Internal server error' });
 	}
-
-	const { email, password } = body.data;
-
-	const [user] = await db.select().from(users).where(eq(users.email, email));
-
-	if (!user) {
-		return reply.status(404).send({ message: 'Email or password invalid!' });
-	}
-
-	const passwordMatches = await compare(password, user.password);
-
-	if (!passwordMatches) {
-		return reply.status(400).send({ message: 'Email or password invalid!' });
-	}
-
-	const token = sign({ name: user.name, email: user.email, issueAt: Date.now() }, env.JWT_SECRET, {
-		expiresIn: '60 * 60',
-	});
-
-	return reply.send({ message: 'Logged In!' }).setCookie('Authorization', `Bearer ${token}`, {
-		httpOnly: true,
-		secure: true,
-	});
 });
 
 app.get('/dashboard', { onRequest: [verifyJwt] }, async (req, reply) => {
